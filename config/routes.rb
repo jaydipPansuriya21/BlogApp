5d92548e04db9401c214061be54892266c0963ec
jaydipPansuriya21 <pansuriyajaydip21@gmail.com>
2021-10-09 21:39:03 +0530

    views file added but rotes file is not up to date

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+  get 'sign_up', to: 'registration#new'
+  get 'sing_in', to: 'session#new'
+  delete 'sign_out', to: 'session#destroy'
+  get 'reset_password', to: 'passwords#edit'
+
+
+
 end
