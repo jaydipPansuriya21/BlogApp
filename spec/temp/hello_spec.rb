955d54c6b743e86f09075e85464e955d2ce36498
jaydipPansuriya21 <pansuriyajaydip21@gmail.com>
2022-01-02 19:04:32 +0530

    rspec example added for referance

--- spec/temp/hello_spec.rb
+++ spec/temp/hello_spec.rb
@@ -1,6 +1,19 @@
-describe 'first test' do
-    it 'should work' do 
-        puts "hello world"
+require 'rails_helper'
+
+RSpec.describe 'Example of comman rspec helper methods' do
+    it '10 should be positive' do 
+        expect(10).to be_positive
+    end
+    it '5 should be greater then 4' do 
+        expect(5).to be > 4
+    end
+    it '-10 should be less then 0' do 
+        expect(-10).to be < 0
+    end
+    it 'equal to 10' do
+        expect(10).to be == 10
+    end
+    it '-10 not equal to 0' do
+        expect(-10).not_to be == 0
     end
 end
-
