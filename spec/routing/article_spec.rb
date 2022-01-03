72066c4a709deedf0120f4579b4d2d8f5bb48829
jaydipPansuriya21 <pansuriyajaydip21@gmail.com>
2022-01-03 21:28:21 +0530

    rspec added for routing

---
+++ spec/routing/article_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe '/article routes' do
+    it 'routes for articles request' do
+        aggregate_failures do
+            expect(get '/articles').to route_to(controller: 'articles', action: 'index')
+            expect(get '/articles').to route_to('articles#index')
+            expect(get '/articles?pages=3').to route_to('articles#index', pages: 3)
+        end
+    end
+end
