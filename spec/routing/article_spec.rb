a458e9ddea3d518112a5dcd8bcd054889241188a
jaydipPansuriya21 <pansuriyajaydip21@gmail.com>
2022-01-03 21:46:39 +0530

    rspec for show page routes

--- spec/routing/article_spec.rb
+++ spec/routing/article_spec.rb
@@ -8,4 +8,11 @@ RSpec.describe '/article routes' do
             expect(get '/articles?pages=3').to route_to('articles#index', pages: '3')
         end
     end
+
+    it 'show page for articles request' do
+        aggregate_failures do
+            expect(get '/articles/1').to route_to(controller: 'articles', action: 'show', id: '1')
+            expect(get '/articles/1').to route_to('articles#show', id: '1')
+        end
+    end
 end
